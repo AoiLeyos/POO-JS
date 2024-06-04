23989f8e4ffdd03db8ba5ef5bae85c552052b5ad
romain <romdz01@gmail.com>
2024-06-04 15:22:40 +0200

    feat(04-abstract-classes)make Html & script JS

---
+++ 04-abstract-classes/script.js
@@ -0,0 +1,26 @@
+'use strict'
+class Animal {
+    FaireDuBruit() {
+        console.log("Un animal fait un bruit générique.")
+    }
+}
+
+class Chien extends Animal {
+    FaireDuBruit() {
+        console.log("Le chien aboie.");
+    }
+}
+
+class Chat extends Animal {
+    FaireDuBruit() {
+        console.log("Le chat miaule.");
+    }
+}
+
+const animal = new Animal();
+const chien = new Chien();
+const chat = new Chat();
+
+animal.FaireDuBruit();
+chien.FaireDuBruit();
+chat.FaireDuBruit();
